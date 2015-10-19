4196e10d72de0c649f20f5894e51d767f38ae8c8
Blazej <blazej1@interia.eu>
2015-10-19 19:01:31 +0200

    improve & fix data loader

--- src/main/java/pl/edu/agh/io/TaxiService.java
+++ src/main/java/pl/edu/agh/io/TaxiService.java
@@ -7,11 +7,16 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
+import org.hibernate.StatelessSession;
 
 import pl.edu.agh.io.pojo.CallType;
 import pl.edu.agh.io.pojo.DayType;
@@ -24,44 +29,57 @@ import pl.edu.agh.io.pojo.Trip;
 public class TaxiService {
 	private static final Logger logger = Logger.getLogger(TaxiService.class);
 	private static final String FILENAME = "src/main/resources/data.csv";
+//	private static final String FILENAME = "src/main/resources/train.csv";
+	private static final AtomicInteger threadWorking = new AtomicInteger(0);
+	
+	private static final int MAX_THREAD_NUM = 20;
+	private static final int MAX_NUMBER_OBJECTS_TO_SAVE = 1000;
 	
     public static void main( String[] args ) {
     	HibernateUtil.getSessionFactory();
-    	
+    	System.out.println(new Date());
     	loadData(FILENAME);
+    	System.out.println(new Date());
     	HibernateUtil.getSessionFactory().close();
-//    	splitRoute(null);
-//    	logger.info("HELLO");
-//       	HibernateUtil.getSessionFactory().close();
     }
     
     
     public static void loadData(String filename){    
     	Date startDate = new Date();
     	try {
-    		Session session = HibernateUtil.getSessionFactory().openSession();
-    		session.beginTransaction();
+    		ExecutorService es = Executors.newFixedThreadPool(MAX_THREAD_NUM);
     		
 			FileReader fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
 			
 			br.readLine(); // skip header
 			
+			List<Trip> trips = new ArrayList<Trip>();
+	    	long tripsSize = 0;
+			
 			int lines = 0;
 			String line;
-			while ( (line = br.readLine()) != null){			
+			while ( (line = br.readLine()) != null){	
+				if (tripsSize > MAX_NUMBER_OBJECTS_TO_SAVE){					
+					es.execute(new SaveHelper(trips));					
+					trips = new ArrayList<Trip>();
+					tripsSize = 0;
+				}				
 				lines += 1;
+				tripsSize += 1;
 				
-				String splited[] = line.split(",", 9);
+				String splited[] = line.replace("\"", "").split(",", 9);
 				
 				String trip_id = splited[0];
 				char call_type = splited[1].charAt(0);
 				
-				Integer origin_call = splited[2].equals("") ? null : Integer.parseInt(splited[2]);
+				Integer origin_call = splited[2].equals("") ? null : Integer.parseInt(splited[2]);				
 				Integer origin_stand = splited[3].equals("") ? null : Integer.parseInt(splited[3]);
+							
 				Integer taxi_id = Integer.parseInt(splited[4]);
 				
 				Date timestamp = getDate(splited[5]);
+				
 				char day_type = splited[6].charAt(0);
 				boolean missing_data = Boolean.parseBoolean(splited[7]);
 				String route = splited[8];
@@ -79,20 +97,32 @@ public class TaxiService {
 					startLat = start.getLatitude();
 					startLong = start.getLongitude();
 					endLat = end.getLatitude();
-					endLong = end.getLongitude();
-					
+					endLong = end.getLongitude();				
 				}
 				
 				Trip trip = new Trip(trip_id, CallType.fromChar(call_type), origin_call, 
 						origin_stand, taxi_id, timestamp, DayType.fromChar(day_type), 
 						missing_data, new Route(route), startLat, startLong, endLat, endLong);
 	
-				session.save(trip);
+				trips.add(trip);
 			}
-			System.out.println("Samples: " + lines);
 			fr.close();
-			session.getTransaction().commit();
-			session.close();
+			
+			if (trips.size() > 0){
+				es.execute(new SaveHelper(trips));
+			}
+	    	System.out.println("Samples " + lines);
+			
+			while( threadWorking.get() > 0){
+				System.out.println("Some threads are working ");	
+				try {
+				    Thread.sleep(1000);
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}
+			}
+			es.shutdown();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -119,5 +149,30 @@ public class TaxiService {
     				new BigDecimal(s[1].trim())  ));
     	}
     	return list;
+    }
+
+    
+    public static class SaveHelper implements Runnable {
+    	private List<Trip> trips;
+    	
+    	public SaveHelper(List<Trip> trips) {
+			this.trips = trips;
+		}
+    	
+		@Override
+		public void run() {
+			threadWorking.incrementAndGet();
+			Session session = HibernateUtil.getSessionFactory().openSession();
+	    	session.beginTransaction();
+	    	
+	    	for (Trip trip : trips)
+	    		session.save(trip);
+	    	
+	    	session.getTransaction().commit();
+	    	session.close();	
+	    	trips.clear();
+	    	trips = null;
+	    	threadWorking.decrementAndGet();
+	    }    	
     }
 }
